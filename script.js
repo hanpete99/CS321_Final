bc8dd4c531feddd9d724d3f93c6bcd85fb91c436
hanpete99 <hanpeterson99@gmail.com>
2021-04-21 11:27:24 -0400

    js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-
+function makeColorValue() {
+    return Math.round(Math.random() * 255);
+  }
+  
+function setButtonColor(button, red, green, blue) {
+    button.setAttribute('style','background-color: rgb(' + red + ',' + green + ',' + blue + ');'
+    );
+  }
+  
+  var buttons = document.getElementsByClassName('colorButton');
+  var heading = document.getElementById('colorValue');
+  var answerMessage = document.getElementById('answer');
+  var answerButton = Math.round(Math.random() * (buttons.length - 1));
+  
+  for (var i = 0; i < buttons.length; i++) {
+  
+  var red = makeColorValue();
+  var green = makeColorValue();
+  var blue = makeColorValue();
+  
+  setButtonColor(buttons[i], red, green, blue);
+  
+  if (i === answerButton) {
+      heading.innerHTML =`(${red}, ${green}, ${blue})`;
+  }
+  
+  buttons[i].addEventListener('click', function(){
+      if (this === buttons[answerButton]) {
+          answerMessage.innerHTML = "Correct!";
+      } else {
+          answerMessage.innerHTML = "Wrong answer! Guess again.";
+      }
+  });
+  
+  }
